e0d17409b3e65b37ebda49fcf727838e630d40b6
MONGCHAW <provabben@gmail.com>
2022-09-02 01:34:14 +0600

    Added Extra Problems

---
+++ Extra/LoopRelatedProblems(1).c
@@ -0,0 +1,21 @@
+/* 
+Make a C program that takes the input of a number and prints the number of digits and the sum of those digit
+Sample Input: 512
+Sample Output: The Number you gave was 512, Digit= 3 , Sum= 8
+*/
+#include<stdio.h>
+int main()
+{
+    int number, digit = 0, sum = 0, temp;
+    printf("Enter a number:");
+    scanf("%d",&number);
+    while(number > 0)
+    {
+        temp = number % 10;
+        sum=sum + temp;
+        number = number / 10;
+        digit = digit+1;
+    }
+    printf("Digit = %d\nSum = %d",digit, sum);
+    return 0;
+}
