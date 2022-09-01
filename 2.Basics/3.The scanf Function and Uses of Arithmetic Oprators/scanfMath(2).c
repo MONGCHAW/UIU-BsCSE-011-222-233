ed4f54d87b9597622075f37a1a3f249fcb81f671
MONGCHAW <provabben@gmail.com>
2022-09-02 04:23:56 +0600

    scanfMath(2).c modified for better readabilty

--- 2.Basics/3.The scanf Function and Uses of Arithmetic Oprators/scanfMath(2).c
+++ 2.Basics/3.The scanf Function and Uses of Arithmetic Oprators/scanfMath(2).c
@@ -2,24 +2,30 @@
 
 int main()
 {
-// Create variables
-    int a, b, addition, multiplication, subtraction, division, remainder ;                 // Uninitialized variable
+  //Variable Initialization
+     int num1,num2;                                                                    //Uninitialized variables for numbers
+     int product, addition, quotient, difference, remainder;                          //Uninitialized variables for the answers
 
-// Inputing the Numbers
-    printf("Input the both the numbers for your arithmatic calculations \n");
-    scanf("%d %d", &a, &b);
+  // Inputing Numbers
+     printf("Enter First numbers: ");
+     scanf("%d",&num1);
 
-//Arithmatic Opparations
-    addition = a + b;
-    subtraction = a - b;
-    multiplication = a * b;
-    division = a / b;
+     printf("Enter Secound numbers: ");
+     scanf("%d",&num2);
 
-// Print variables
-    printf("The addition of the numbers are %d\n\n", addition);
-    printf("The subtraction of the numbers are %d\n\n", subtraction);
-    printf("The multiplication of the numbers are %d\n\n", multiplication);
-    printf("The division of the numbers are %d\n\n", division);
+  //Arithmatic Oparations
+    addition = num1 + num2;
+    difference = num1 - num2;
+    product = num1 * num2;
+    quotient = num1 / num2;
+    remainder = num1 % num2;
 
-   return 0;
-}
+  //Printing the Answers
+    printf("%d + %d = %d\n", num1, num2, addition);
+    printf("%d - %d = %d\n", num1, num2, difference);
+    printf("%d * %d = %d\n", num1, num2, product);
+    printf("%d / %d = %d\n", num1, num2, quotient);
+    printf("%d %% %d = %d\n", num1, num2, remainder);
+
+  return 0;
+ }
