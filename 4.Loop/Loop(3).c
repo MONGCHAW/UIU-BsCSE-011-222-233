3ca853fe9614ec74f23b05c8bf9b12f641ce3126
Provab Mongchaw Khyang <59840759+MONGCHAW@users.noreply.github.com>
2022-09-09 20:21:04 +0600

    added loop(3) solve

--- 4.Loop/Loop(3).c
+++ 4.Loop/Loop(3).c
@@ -1,28 +1,19 @@
-  #include <stdio.h>
+#include <stdio.h>
 
-  int main()
-  {
-  // Create variables
+int main()
+{
+// Create variables
       int i, n;                 // Uninitialized variable
 
-  // Inputing the Numbers
+// Inputing the Numbers
+      system("cls");
       printf("Enter the Nth numbers:");
       scanf("%d", &n);
 
-  //Logics
-      for(i=0;n>0;i++)    
-      {    
-        a[i]=n%2;    
-        n=n/2;    
-      }
-        printf("\nBinary of Given Number is=");    
-      for(i=i-1;i>=0;i--)    
-      {    
-        printf("%d",a[i]);    
-      }    
-    // for (i = 1; i <= n; i = i+2)
-    // {
-    //   printf("%d ", i);
-    // }
-      return 0;
-  }
+//Logics
+  for (i = 1; i <= n; ++i)
+    {
+      printf("%d, ",(int)(i%2));
+    }
+return 0;
+}
