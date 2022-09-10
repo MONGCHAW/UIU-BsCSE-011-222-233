48a987fb455f9927bb251d597f4d83dbb9c0df4e
Provab Mongchaw Khyang <59840759+MONGCHAW@users.noreply.github.com>
2022-09-10 17:16:15 +0600

    fixed loop(18).c

--- 4.Loop/Loop(18).c
+++ 4.Loop/Loop(18).c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 int main() {
-    int n, i;
+    int n, i,b;
     unsigned long long fact = 1;
     printf("Enter an integer: ");
     scanf("%d", &n);
 
+    printf("%d != %d", n, n);
     // shows error if the user enters a negative integer
     if (n < 0)
-        printf("Error! Factorial of a negative number doesn't exist.");
+      printf("Error! Factorial of a negative number doesn't exist.");
     else {
         for (i = 1; i <= n; ++i) {
             fact *= i;
+            b = n-i;
+        if (b >0)
+            {
+            printf(" x %d ", b);
+            }
         }
-        printf("Factorial of %d = %llu", n, fact);
     }
-
+    printf("= %llu", fact);
     return 0;
 }
+;
