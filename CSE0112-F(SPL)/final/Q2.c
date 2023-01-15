057b417c10136f999cded9718df7e9fdd6cb281b
Provab Mongchaw Khyang <59840759+MONGCHAW@users.noreply.github.com>
2023-01-15 16:07:11 +0600

    Create Q2.c

---
+++ CSE0112-F(SPL)/final/Q2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int fibonacciSeries (int a){
+    int n1=0,n2=1,n3;
+    if(n > 0){
+        n3 = n1 + n2;
+        n1 = n2;
+        n1 = n3;
+        printf("%d",n3);
+if ( i = 0 )
+                printf(" & ");
+            else if ( i % 2 != 0)
+                printf(" # ");
+            else if (i % 2 == 0)
+                printf(" $ ");
+
+        fibonacciSeries(n-1);
+
+    }
+ }
+
+void main(){
+    int n;
+
+    scanf("%d " &n);
+    fibonacciSeries(n-2);
+                }
+
